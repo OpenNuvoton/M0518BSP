1ec6e7975b195debcedc4da434cd298c9648bc70
MS20 WLChen1 <wlchen1@nuvoton.com>
2025-05-15 14:03:34 +0800

    1. Added read/write byte related functions to the I2C driver. 2. Fixed ISP_I2C  - Disabled I2C IRQ until ParseCmd() is completed to prevent ISP from triggering an interrupt and fetching data prematurely.
    Change-Id: I1d3f8ed7481d0460aa6d89bfcf2f495f5f23f18d

--- Library/StdDriver/inc/i2c.h
+++ Library/StdDriver/inc/i2c.h
@@ -52,9 +52,14 @@ extern "C"
 #define I2C_GCMODE_ENABLE           1      /*!< Enable  I2C GC Mode                                                       */
 #define I2C_GCMODE_DISABLE          0      /*!< Disable I2C GC Mode                                                       */
 
-#define I2C_TIMEOUT                 SystemCoreClock /*!< IC time-out counter (1 second time-out)                          */
+#define I2C_TIMEOUT                 SystemCoreClock /*!< I2C time-out counter (1 second time-out)                         */
+#define I2C_OK                      ( 0L)  /*!< I2C operation OK                                                          */
+#define I2C_ERR_FAIL                (-1L)  /*!< I2C operation failed                                                      */
+#define I2C_ERR_TIMEOUT             (-2L)  /*!< I2C operation abort due to timeout error                                  */
 
 /*@}*/ /* end of group M0518_I2C_EXPORTED_CONSTANTS */
+
+extern int32_t g_I2C_i32ErrCode;
 
 /** @addtogroup M0518_I2C_EXPORTED_FUNCTIONS I2C Exported Functions
   @{
@@ -202,6 +207,18 @@ void I2C_DisableTimeout(I2C_T *i2c);
 void I2C_EnableWakeup(I2C_T *i2c);
 void I2C_DisableWakeup(I2C_T *i2c);
 void I2C_SetData(I2C_T *i2c, uint8_t u8Data);
+uint8_t I2C_WriteByte(I2C_T *i2c, uint8_t u8SlaveAddr, const uint8_t data);
+uint32_t I2C_WriteMultiBytes(I2C_T *i2c, uint8_t u8SlaveAddr, const uint8_t *data, uint32_t u32wLen);
+uint8_t I2C_WriteByteOneReg(I2C_T *i2c, uint8_t u8SlaveAddr, uint8_t u8DataAddr, const uint8_t data);
+uint32_t I2C_WriteMultiBytesOneReg(I2C_T *i2c, uint8_t u8SlaveAddr, uint8_t u8DataAddr, const uint8_t *data, uint32_t u32wLen);
+uint8_t I2C_WriteByteTwoRegs(I2C_T *i2c, uint8_t u8SlaveAddr, uint16_t u16DataAddr, const uint8_t data);
+uint32_t I2C_WriteMultiBytesTwoRegs(I2C_T *i2c, uint8_t u8SlaveAddr, uint16_t u16DataAddr, const uint8_t *data, uint32_t u32wLen);
+uint8_t I2C_ReadByte(I2C_T *i2c, uint8_t u8SlaveAddr);
+uint32_t I2C_ReadMultiBytes(I2C_T *i2c, uint8_t u8SlaveAddr, uint8_t *rdata, uint32_t u32rLen);
+uint8_t I2C_ReadByteOneReg(I2C_T *i2c, uint8_t u8SlaveAddr, uint8_t u8DataAddr);
+uint32_t I2C_ReadMultiBytesOneReg(I2C_T *i2c, uint8_t u8SlaveAddr, uint8_t u8DataAddr, uint8_t *rdata, uint32_t u32rLen);
+uint8_t I2C_ReadByteTwoRegs(I2C_T *i2c, uint8_t u8SlaveAddr, uint16_t u16DataAddr);
+uint32_t I2C_ReadMultiBytesTwoRegs(I2C_T *i2c, uint8_t u8SlaveAddr, uint16_t u16DataAddr, uint8_t *rdata, uint32_t u32rLen);
 
 /*@}*/ /* end of group M0518_I2C_EXPORTED_FUNCTIONS */
 
